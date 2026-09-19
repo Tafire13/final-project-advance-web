7c009738cd1e7b03c102b165bfb1a50cacabfb1f
TsumiyaZ <watcharakornnigc@gmail.com>
2026-09-19 22:20:27 +0700

    Update dbconnect.ts

--- backend/config/dbconnect.ts
+++ backend/config/dbconnect.ts
@@ -1,10 +1,12 @@
 import { createPool } from 'mysql2/promise';
 
+const pass = "AVNS_1c4D8MrawSBWx8W5cfF";
+
 export const conn = createPool({
     connectionLimit: 10,
     host: 'mysql-web-final-project-watcharakoon2535-fa27.c.aivencloud.com',
     port: 10051,
     user: 'avnadmin',
-    password: '[รหัสผ่าน]',
+    password: `${pass}`,
     database: 'lunch_delivery'
 });
