fdcdb0729261bde10a80f5319661dfec29c30f93
TsumiyaZ <watcharakornnigc@gmail.com>
2026-09-19 16:13:20 +0700

    change file

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('final-project-advance-web');
+  protected readonly title = signal('frontend');
 }
